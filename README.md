58772d250fbcdec51edd77cf639ac354747adbb3
dzarlax <alexey@panfilov.net>
2021-06-17 21:14:08 +0300

    Make it easier =)

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ To start, you need to set environment settings:
 
 1) bot_token (without bot in the beginning)
    
-2) chat_id
+2) chat_id (chat id of yor notification chat/group)
+
+3) interval (how often to check)
 
 For persistent storage, direct working_dir to your working directory.
--- main.py
+++ main.py
@@ -1,73 +1,52 @@
-import cmd
+import schedule
 from os import listdir
 from os.path import isfile, join
-import schedule
 import requests
 import os
+import time
 
 def convert_avi_to_mp4(avi_file_path, output_name):
-    os.popen(
+    print('converting')
+    os.system(
         "ffmpeg -i '{input}' -ac 2 -b:v 2000k -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 '{output}.mp4'".format(
             input=avi_file_path, output=output_name))
+    return True
 
-class UpdateFilesNotification(cmd.Cmd):
-    def __init__(self):
-        super(UpdateFilesNotification, self).__init__()
-        self.folder = '/working_dir'
-        self.seconds = 1
-        if not os.path.exists('logs.txt'):
-            open('logs.txt', 'w').close()
+def sendToTelegram( file_name):
+    print('file_name', file_name)
+    file = folder + "/" + file_name
+    convert_avi_to_mp4(file, file)
+    video = file + '.mp4'
+    message = ('https://api.telegram.org/' + bot_token + '/sendDocument?chat_id=' + chat_id)
+    send = requests.post(message, files={
+        'document': open(video, 'rb')
+    })
+    print(send.status_code, send.reason, send.content)
+    os.remove(file +'.mp4')
 
-    def do_set_folder(self, line):
-        if not line:
-            line = self.folder
-        else:
-            self.folder = line
-        print("Set folder: ", self.folder)
-
-    def do_set_time_interval(self, line):
-        if not line:
-            line = self.seconds
-        else:
-            self.seconds = line
-        print("running script every - ", self.seconds, "seconds")
-
-    def do_show_settings(self, line):
-        print("Settings:\nFolder:", self.folder, "\nStart every:", self.seconds, 'seconds')
-
-
-    def run(self):
-        onlyfiles = [f for f in listdir(self.folder) if isfile(join(self.folder, f))]
-        for file in onlyfiles:
-            print(file)
-            with open('logs.txt') as f:
-                if file not in f.read():
-                    file1 = open("logs.txt", "a")
-                    file1.writelines(file)
-                    file1.close()
-                    print("file --", file)
-                    if file.endswith('.avi'):
-                        self.sendToTelegram(file)
-
-    def sendToTelegram(self, file_name):
-        print('file_name', file_name)
-        bot_token='bot'+ os.environ['bot_token']
-        chat_id=os.environ['chat_id']
-        file = self.folder + "/" + file_name
-        convert_avi_to_mp4(file, file)
-        video = file + '.mp4'
-        message = ('https://api.telegram.org/' + bot_token + '/sendDocument?chat_id=' + chat_id)
-        send = requests.post(message, files={
-            'document': open(video, 'rb')
-        })
-        print(send.status_code, send.reason, send.content)
-        os.remove(file +'.mp4')
-
-    def do_job(self, line):
-        schedule.every(self.seconds).seconds.do(self.run)
-        while True:
-            schedule.run_pending()
+def main():
+    if not os.path.exists('logs.txt'):
+        open('logs.txt', 'w').close()
+    onlyfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
+    for file in onlyfiles:
+        print(file)
+        with open('logs.txt') as f:
+            if file not in f.read():
+                file1 = open("logs.txt", "a")
+                file1.writelines(file)
+                file1.close()
+                print("file --", file)
+                if file.endswith('.avi'):
+                    sendToTelegram(file)
 
 
-if __name__ == '__main__':
-    UpdateFilesNotification().cmdloop()
+folder = '/Users/alexpanfilov/Documents/test'
+bot_token='bot'+ os.environ['bot_token']
+chat_id=os.environ['chat_id']
+seconds = os.environ['interval']
+
+schedule.every(seconds).seconds.do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
